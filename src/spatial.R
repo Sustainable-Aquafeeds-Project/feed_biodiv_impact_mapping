4f0551b8ac37923f2dfaa984c1455fa6164d986a
gclawson1 <clawsonsa1101@gmail.com>
2023-06-29 23:08:25 +0000

    checked over mapspam scaling

--- src/spatial.R
+++ src/spatial.R
@@ -1,4 +1,5 @@
 
+library(terra)
 
 #projections
 
@@ -9,32 +10,31 @@ equal_area_moll_projstring <- "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +d
 equal_area_moll_esri <- "ESRI:54009"
 
 
-
-library(raster)
-library(sp)
-
-
-## to source:
-## source('https://raw.githubusercontent.com/cdkuempel/food_chicken_salmon/master/_spatial/template_raster.R?token=ABLMCDNSCGBY3NWPCDJRISC5JA4JI')
-
-
 food_raster <- raster(nrows=2160, ncols=4320, xmn=-180, xmx=180, ymn=-90, ymx=90)
 food_crs <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
 
-raster_df <- function(input_raster){ #input_raster=food_raster
-  template_rast <- raster(nrows=2160, ncols=4320, xmn=-180, xmx=180, ymn=-90, ymx=90)
-  values(template_rast) <- 1:ncell(food_raster)
-  template_df <- as.data.frame(template_rast, xy=TRUE)
-  names(template_df)[3] <- "cellindex"
-  if(ncell(input_raster) == ncell(template_rast)){
-    final_rast <- cbind(template_df, as.data.frame(input_raster))
-    return(final_rast)
-  } else
-    stop("Rasters are different sizes")
-  
-}
+## make a mollweide 10km by 10km raster 
+
+# Define the dimensions
+nrow <- 1755
+ncol <- 3510
+nlyr <- 1
+
+# Define the resolution
+x_res <- 10279.27
+y_res <- 10279.27
+
+# Define the extent
+xmin <- -18040095
+xmax <- 18040134
+ymin <- -9020047
+ymax <- 9020067
+
+# Define the coordinate reference system (CRS)
+crs <- "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs"
+
+# Create the raster template
+moll_template <- rast(nrow=nrow, ncol=ncol, nlyr=nlyr, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax,
+          res=c(x_res, y_res), crs=crs)
 
 
-fake_raster <- raster(nrows=2160, ncols=4320, xmn=-180, xmx=180, ymn=-89.999, ymx=90)
-values(fake_raster) <- 1000
-check <- raster_df(fake_raster)
